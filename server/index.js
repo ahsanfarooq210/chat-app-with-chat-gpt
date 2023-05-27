8ba5fa3ca04f8ce3cab9d1cc0191bf6979f70a8b
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2023-05-27 11:19:58 -0700

    configuring the backend server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import {Configuration,OpenAIApi} from 'openai'
+import helmet from "helmet";
+import morgan from "morgan";
+
+// configurations
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors())
+
+// open ai setup
+const configuration= new Configuration({
+    apiKey:process.env.OPEN_API_KEY
+})
+export const openAi=new OpenAIApi(configuration)
+
+// server setup
+const PORT = process.env.PORT||9000;
+app.listen(PORT,()=>{
+    console.log(`example app listening at http : //localhost:${PORT}`)
+})
